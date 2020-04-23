cc268d171aaccea000e9d7bb825228b34d034758
devminchan <minchan.dev@gmail.com>
2020-04-23 17:25:35 +0900

    User 생성 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Body, Post } from '@nestjs/common';
 import { CreateUserDto } from './user.dto';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
+  async createUser(@Body() createUserDto: CreateUserDto) {
     console.log('user dto:', createUserDto);
 
-    return createUserDto;
+    const result = await this.userService.createUser(createUserDto);
+
+    return result;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
-  
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create({
+      userId: createUserDto.userId,
+      password: createUserDto.password,
+    });
+
+    await this.userRepository.save(user);
+
+    return user;
+  }
 }
