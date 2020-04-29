03b2a5383590397e91996f6652c8d0c3515e3a72
devminchan <minchan.dev@gmail.com>
2020-04-29 16:30:17 +0900

    dto에 @ApiProperty 적용

--- src/cat-post/cat-post.dto.ts
+++ src/cat-post/cat-post.dto.ts
@@ -1,18 +1,23 @@
 import { IsNotEmpty, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @ApiProperty()
   @IsNotEmpty()
   content: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsUrl()
   imageUrl: string;
 }
 
 export class UpdatePostDto {
+  @ApiProperty()
   @IsNotEmpty()
   content: string;
-
+  
+  @ApiProperty()
   @IsNotEmpty()
   @IsUrl()
   imageUrl: string;
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,16 +1,20 @@
 import { Length, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @Length(4, 40)
   userId: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @Length(8)
   password: string;
 }
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @Length(8)
   password: string;
