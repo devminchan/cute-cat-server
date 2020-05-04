0d2e60a407d145d50f563f6cf8d40fc607648581
devminchan <minchan.dev@gmail.com>
2020-05-04 13:45:13 +0900

    entity에 @ApiResponseProperty 적용

--- src/cat-post/cat-post.entity.ts
+++ src/cat-post/cat-post.entity.ts
@@ -1,18 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { TimestampEntity } from 'src/utils/timestamp.entity';
 import { User } from 'src/user/user.entity';
+import { ApiResponseProperty } from '@nestjs/swagger';
 
 @Entity()
 export class CatPost extends TimestampEntity {
+  @ApiResponseProperty()
   @PrimaryGeneratedColumn({ name: 'seq_no' })
   seqNo: number;
 
+  @ApiResponseProperty()
   @Column({ nullable: false })
   content: string;
 
+  @ApiResponseProperty()
   @Column({ name: 'image_url', nullable: false })
   imageUrl: string;
 
+  @ApiResponseProperty()
   @Column({ name: 'post_url', nullable: true })
   postUrl: string;
 
--- src/utils/timestamp.entity.ts
+++ src/utils/timestamp.entity.ts
@@ -1,9 +1,12 @@
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { ApiResponseProperty } from '@nestjs/swagger';
 
 export abstract class TimestampEntity {
+  @ApiResponseProperty()
   @CreateDateColumn({ name: 'created_at' })
   createdDate: Date;
 
+  @ApiResponseProperty()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedDate: Date;
 }
