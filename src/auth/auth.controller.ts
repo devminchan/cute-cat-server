f3de9f55b92155cb5b4a7134922266d47010544b
devminchan <minchan.dev@gmail.com>
2020-05-04 13:59:52 +0900

    Auth controller 로그인 response 설명 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 import { LoginRequest } from './auth.dto';
+import { LoginResult } from './auth.types';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -12,6 +13,10 @@ export class AuthController {
   @ApiBody({ type: LoginRequest })
   @ApiOperation({
     description: 'id, password로 유저 로그인(jwt 토큰 발급)',
+  })
+  @ApiResponse({
+    status: 201,
+    type: LoginResult
   })
   @UseGuards(AuthGuard('local'))
   @Post('login')
